eb4e54d506ca0cb9ccc897185d6fbceda7aad55f
Jason Haimerl <jason@Haimerl.us>
2024-07-08 20:52:11 -0500

    Implement deep clone function

--- js/src/index.js
+++ js/src/index.js
@@ -3,6 +3,25 @@
     var anti = {};
 
 
+    anti.cloneDeep = function(obj) {
+        if (obj === null || typeof obj !== 'object') return obj;
+        if (obj instanceof Date) return new Date(obj.getTime());
+        if (obj instanceof Array) {
+            var clonedArr = [];
+            for (var i = 0; i < obj.length; i++) {
+                clonedArr[i] = anti.cloneDeep(obj[i]);
+            }
+            return clonedArr;
+        }
+        var clonedObj = {};
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                clonedObj[key] = anti.cloneDeep(obj[key]);
+            }
+        }
+        return clonedObj;
+    };
+
 
 
     global.anti = anti;
