4c662bc2c6268011f1b24e8856ac083b7bc21d59
Jason Haimerl <jason@Haimerl.us>
2024-07-09 20:53:01 -0500

    implement deep equality check function

--- js/src/index.js
+++ js/src/index.js
@@ -156,5 +156,32 @@
         return rangeArray;
     };
 
+
+    anti.isEqual = function(obj1, obj2) {
+        if (obj1 === obj2) return true;    
+        if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
+            return false;
+        }    
+        if (Object.prototype.toString.call(obj1) !== Object.prototype.toString.call(obj2)) {
+            return false;
+        } 
+        var keys1 = Object.keys(obj1);
+        var keys2 = Object.keys(obj2);    
+        if (keys1.length !== keys2.length) return false;    
+        for (var key of keys1) {
+            if (!keys2.includes(key)) {
+                return false;
+            }    
+            if (typeof obj1[key] === 'object' && obj1[key] !== null && typeof obj2[key] === 'object' && obj2[key] !== null) {
+                if (!antiquity.isEqual(obj1[key], obj2[key])) {
+                    return false;
+                }
+            } else if (obj1[key] !== obj2[key]) {
+                return false;
+            }
+        }
+        return true;
+    };
+
     global.anti = anti;
 })(this);
